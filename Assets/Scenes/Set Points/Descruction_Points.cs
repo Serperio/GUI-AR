85af0c61b3dd85b860a86624c02e2234ea5c7a5e
Serperio <serperiox@gmail.com>
2023-06-28 17:28:16 -0400

    pathings

--- Assets/Scenes/Set Points/Descruction_Points.cs
+++ Assets/Scenes/Set Points/Descruction_Points.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Descruction_Points : MonoBehaviour
 {
+    GameObject flecha;
+    private void Start()
+    {
+        flecha = GameObject.Find("Arrow");
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("MainCamera"))
         {
         Destroy(gameObject);
+        flecha.GetComponent<ArrowRotation>().DeletePoint();
         }
     }
 
