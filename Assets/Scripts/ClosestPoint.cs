8d5f038fc2f8d7b4b2ab9c133e9a962da1d74bc0
josiashrojas <josias.h.rojas@gmail.com>
2023-08-29 18:21:16 -0400

    feature: calcular punto actual

--- Assets/Scripts/ClosestPoint.cs
+++ Assets/Scripts/ClosestPoint.cs
@@ -22,52 +22,69 @@ public class ClosestPoint : MonoBehaviour
       
     private IEnumerator NearestPoint()
     {
-        api.NearbyPointsAPI();
+        yield return new WaitForSeconds(10f);
+        GameObject.Find("AppManager").GetComponent<API>().NearbyPointsAPI();
         float[] positions = gpsloc.GetLastPosition();
         float latitude = positions[0]; //Pasar
         float longitude = positions[1]; //Pasar de gps
-        yield return new WaitForSeconds(10f);
         Point nearestPoint = null;
         double shortestDistance = Mathf.Infinity;
         pointList1 = api._pointlist; 
-        closestPointGUI.text=pointList1.Count.ToString();
+        closestPointGUI.text= api._pointlist.Count.ToString();
+        pointList = new List<Point>();
 
 
-            foreach (Point point in pointList1){      
-                if (point.floor.ToString() == api.pred.prediction.ToString())
-                {
-                    pointList.Add(point);
-                } 
+        foreach (Point point in pointList1){      
+            if (point.floor.ToString() == api.pred.prediction.ToString())
+            {
+                pointList.Add(point);
+            } 
                 
-            }
+        }
 
         foreach (Point point in pointList)
-            {
-                double distance = CalculateDistance(latitude, longitude, point.x, point.y);
+        {
 
+            double distance = CalculateDistance(latitude, longitude, point.x, point.y);
             if (distance < shortestDistance)
-                {
+            {
                 shortestDistance = distance;
                 nearestPoint = point;
-                }
             }
+        }
         //closestPointGUI.text = "Entro+2";
         if (nearestPoint != null)
-            {
-                closestPointGUI.text = "En: " + nearestPoint.name;
-            }
-            else
-            {
-                //closestPointGUI.text = "No se conoce la ubicación";
-            }
-            StartCoroutine(NearestPoint());
+        {
+            closestPointGUI.text = "En: " + nearestPoint.name;
+        }
+        else
+        {
+            //closestPointGUI.text = "No se conoce la ubicación";
+        }
+        StartCoroutine(NearestPoint());
     }
 
-        private double CalculateDistance(double lat1, double lon1, double lat2, double lon2)
+    private double CalculateDistance(double lat1, double lon1, double lat2, double lon2)
     {
         double distance = Mathf.Sqrt(Mathf.Abs((float)((lat2 - lat1) * (lat2 - lat1) + (lon2 - lon1) * (lon2 - lon1))));
         return distance;
     }
+
+    private void _ShowAndroidToastMessage(string message)
+    {
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        if (unityActivity != null)
+        {
+            AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+            unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+            {
+                AndroidJavaObject toastObject = toastClass.CallStatic<AndroidJavaObject>("makeText", unityActivity, message, 0);
+                toastObject.Call("show");
+            }));
+        }
     }
+}
 
 
