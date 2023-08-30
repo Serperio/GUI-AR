ac2e99ca1d304e9ef3163405f7360bb4b5327e62
jbecker <jbecker@alumnos.inf.utfsm.cl>
2023-08-30 03:29:50 +0200

    Inicio UI Ruta Actual

---
+++ Assets/Scripts/RutaActual/RutaActualHandler.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RutaActualHandler : MonoBehaviour
+{
+    List<ElementoPunto> listaPuntos = new List<ElementoPunto> ();
+    //[serializeField] int maxCount = 11; //Este es el max de elemento a mostrar, de momento no lo tomaremos en cuenta
+
+    private void CargarPunto(){
+
+    }
+
+    private void GuardarPunto(){
+        
+    }
+}
