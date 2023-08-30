491f368a5bc02bb77aa4680dbd5402070c48a9f8
jbecker <jbecker@alumnos.inf.utfsm.cl>
2023-08-30 03:29:50 +0200

    Inicio UI Ruta Actual

---
+++ Assets/Scripts/RutaActual/ElementoPunto.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class ElementoPunto
+{
+    public string nombrePunto;
+
+    public ElementoPunto(string nombre){
+        this.nombrePunto = nombre;
+    }
+}
