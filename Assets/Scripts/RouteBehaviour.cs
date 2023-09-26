b8b5dc14f6486bff663ac628d7dbc79e3499dcd8
PABLO BETANCOURT <pablo.betancourt@usm.cl>
2023-09-26 15:06:41 -0300

    modifica puntos

---
+++ Assets/Scripts/RouteBehaviour.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ARLocation.MapboxRoutes
+{
+    public class RouteBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject route;
+        [SerializeField]
+        GameObject MapBox;
+        // Start is called before the first frame update
+        public void routeOn()
+        {
+            route.SetActive(true);
+            MapBox.SetActive(true);
+            //MapBox.GetComponent<MapboxRoute>().LoadCustomRoute(route.GetComponent<CustomRoute>());
+        }
+
+    }
+}
